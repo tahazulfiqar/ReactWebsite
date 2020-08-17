d185c929e10bf8e35ab0e7a57bd7892f5d760a2c
tahazulfiqar <taha.zulfiqar@mail.utoronto.ca>
2020-08-17 19:23:49 -0400

    Added copyright text in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,29 +4,36 @@ import { SocialIcon } from "react-social-icons";
 
 export const Footer = () => {
   return (
-    <div
-      className="border-top"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "1%",
-      }}
-    >
-      <div style={{ paddingRight: 5 }}>
-        <SocialIcon
-          url="https://www.linkedin.com/in/tahazulfiqar/"
-          style={{ height: 45, width: 45 }}
-          bgColor="black"
-        />
-        <SocialIcon
-          url="https://github.com/tahazulfiqar"
-          style={{
-            height: 45,
-            width: 45,
-          }}
-          bgColor="Black"
-        />
+    <div>
+      <div
+        className="border-top"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "1%",
+        }}
+      >
+        <div style={{ paddingRight: 5 }}>
+          <SocialIcon
+            url="https://www.linkedin.com/in/tahazulfiqar/"
+            style={{ height: 45, width: 45 }}
+            bgColor="black"
+          />
+          <SocialIcon
+            url="https://github.com/tahazulfiqar"
+            style={{
+              height: 45,
+              width: 45,
+            }}
+            bgColor="Black"
+          />
+        </div>
+      </div>
+      <div className="row text-center">
+        <p className="col-sm">
+          {" "}
+          Copyright &copy;{new Date().getFullYear()} Taha Zulfiqar
+        </p>
       </div>
     </div>
   );
